f409faa8261aa2bb527af3ba4cb66e7d7abc27d1
mironrybkin <miron.ribkin98@mail.ru>
2023-04-10 16:11:17 +0300

    new backend

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.use("/api/users", require("./src/routes/users"));
 app.use("/api/messages", require("./src/routes/messages"));
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-        });
-        app.listen(process.env.PORT, () =>
-            console.log(`Started on port ${process.env.PORT}...`)
-        );
-    } catch (e) {
-        console.log("Server error", e.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+    });
+    app.listen(process.env.PORT, () =>
+      console.log(`Started on port ${process.env.PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server error", e.message);
+    process.exit(1);
+  }
 };
 
 start();
